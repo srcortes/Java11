d80d76b06951760977c1d3a1d13c5dbb91794e47
srcortes <john.rodriguez.cortes@gmail.com>
2025-08-19 17:56:22 -0500

    practice

--- src/main/java/MODERNJAVA/PART2/SPLITITERATOR/ReverseStringSplit.java
+++ src/main/java/MODERNJAVA/PART2/SPLITITERATOR/ReverseStringSplit.java
@@ -2,16 +2,45 @@ package MODERNJAVA.PART2.SPLITITERATOR;
 
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 public class ReverseStringSplit implements Spliterator<String> {
+
+    private final String str;
+    private final int chunkSize;
+    private int currentPos = 0;
+
+    public ReverseStringSplit(String str, int chunkSize) {
+        this.str = str;
+        this.chunkSize = chunkSize;
+    }
+
     @Override
     public boolean tryAdvance(Consumer<? super String> action) {
-        return false;
+        if (currentPos >= str.length()) {
+            return false; // no more chunks
+        }
+        int endPos = Math.min(currentPos + chunkSize, str.length());
+        String chunk = str.substring(currentPos, endPos);
+
+        action.accept(chunk);
+        currentPos = endPos;
+        return true;
     }
 
     @Override
     public Spliterator<String> trySplit() {
-        return null;
+
+        System.out.println("******* TrySplit ******");
+        int remaining = str.length() - currentPos;
+
+
+        if (remaining < 3) return null; // Only split if enough left
+        int splitPoint = currentPos + remaining / 2;
+        ReverseStringSplit split = new ReverseStringSplit(str, chunkSize);
+        this.currentPos = splitPoint;
+        return split;
     }
 
     @Override
@@ -22,5 +51,12 @@ public class ReverseStringSplit implements Spliterator<String> {
     @Override
     public int characteristics() {
         return 0;
+    }
+
+    public static void main(String[] args) {
+        ReverseStringSplit reverseStringSplit = new ReverseStringSplit("John Jairo", 4);
+
+       StreamSupport.stream(reverseStringSplit, true)
+               .forEach(System.out::println);
     }
 }
